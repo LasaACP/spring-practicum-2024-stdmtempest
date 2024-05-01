96ad610dc7a9715d6dec56a58ce63def209a1d9b
Ram Sivaraman <11295719-RamSivaraman1@users.noreply.replit.com>
2024-05-01 15:41:25 +0000

    delete extra version control text

--- main.cpp
+++ main.cpp
@@ -38,30 +38,5 @@ int main()
 
 
         return 0;
-
-
-
-
-
-<<<<<<< HEAD
-=======
-int main() {
-    std::cout << "Hello World!";
-    std::cout << "Getting Started now!";
-    std::cout << "Gavin commit";
-    std::cout << "Nifemi commit test" << std::endl;
-    std::cout << "Getting setup on Visual Studio - Test" << std::endl;
-    return 0;
->>>>>>> refs/remotes/origin/main
+  
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
