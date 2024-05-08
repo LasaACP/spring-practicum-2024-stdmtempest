83f1a5d16841d6df5ca2a65a0dbf99efa9c50034
Ram Sivaraman <11295719-RamSivaraman1@users.noreply.replit.com>
2024-05-08 15:43:35 +0000

    Added ability to read CSV

--- WeatherDataExtraction.cpp
+++ WeatherDataExtraction.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int CSVlength(int order_num) {
@@ -20,11 +21,10 @@ int CSVlength(int order_num) {
   return -1;
 }
 
-string** CSVarray(int order_num) {
+void CSVarray(int order_num, vector<vector<string>> &v) {
   string csv_file_name = "weather-data/" + to_string(order_num) + ".csv";
   int length = CSVlength(order_num);
   string line;
-  string ** output = new string * [length];
   ifstream csv_file(csv_file_name);
   getline(csv_file, line);
   int num_cols = 1; //starting with 1 because the final column doesn't have a comma after it
@@ -33,22 +33,30 @@ string** CSVarray(int order_num) {
       num_cols++;
     }
   }
-  for (int i = 0; i < length; i++) {
-    output[i] = new string[num_cols];
-  }
-
-  //Array has been created, now time to populate
+  
   csv_file.close();
   csv_file.open(csv_file_name);
-
-  for (int i = 0; i < length; i++) {
-    
-  }
   
-  return output;
+  for (int i = 0; i < length; i++) {
+    vector<string> row;
+    getline(csv_file, line);
+    for (int j = 0; j < num_cols; j++) {
+      if (line[0] != '\"') {
+        row.push_back(line.substr(0, line.find(",")));
+        line = line.substr(line.find(",") + 1);
+      }
+      else {
+        line = line.substr(1);
+        row.push_back(line.substr(0, line.find("\"")));
+        line = line.substr(line.find("\"") + 1);
+      }
+    }
+    v.push_back(row);
+  }
 }
 
 int main() {
   cout << CSVlength(3674657) << endl;
-  string ** array = CSVarray(3674657);
+  vector<vector<string>> csv_array;
+  CSVarray(3674657, csv_array);
 }
