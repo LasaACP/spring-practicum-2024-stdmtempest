1a699a5a723972858c543f45d5f8befa303d25d1
Ram Sivaraman <11295719-RamSivaraman1@users.noreply.replit.com>
2024-05-08 15:05:54 +0000

    Update CSV extraction

--- WeatherDataExtraction.cpp
+++ WeatherDataExtraction.cpp
@@ -20,6 +20,35 @@ int CSVlength(int order_num) {
   return -1;
 }
 
+string** CSVarray(int order_num) {
+  string csv_file_name = "weather-data/" + to_string(order_num) + ".csv";
+  int length = CSVlength(order_num);
+  string line;
+  string ** output = new string * [length];
+  ifstream csv_file(csv_file_name);
+  getline(csv_file, line);
+  int num_cols = 1; //starting with 1 because the final column doesn't have a comma after it
+  for (int i = 0; i < line.length(); i++) {
+    if (line[i] == ',') {
+      num_cols++;
+    }
+  }
+  for (int i = 0; i < length; i++) {
+    output[i] = new string[num_cols];
+  }
+
+  //Array has been created, now time to populate
+  csv_file.close();
+  csv_file.open(csv_file_name);
+
+  for (int i = 0; i < length; i++) {
+    
+  }
+  
+  return output;
+}
+
 int main() {
   cout << CSVlength(3674657) << endl;
+  string ** array = CSVarray(3674657);
 }
